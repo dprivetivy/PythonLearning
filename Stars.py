23c356de9c5dfd6d427deed61d9bbe9a6120b6eb
Daniel Privetivy <75144718+dprivetivy@users.noreply.github.com>
2023-04-15 20:55:07 +0200

    Create Stars.py

---
+++ Stars.py
@@ -0,0 +1,9 @@
+pocet_hvezd = input("Kolik řádků hvězd chceš?")
+
+pocet_hvezd = int(pocet_hvezd)
+
+kvantita_hvezd = "*"
+
+for i in range(pocet_hvezd):
+    print(kvantita_hvezd)
+    kvantita_hvezd += "*"
